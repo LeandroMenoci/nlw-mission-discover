773649647ea48e149ef810afedf14077c163d9dd
LeandroMenoci <menoci.leandro@gmail.com>
2022-02-21 18:08:07 -0300

    iniciado todas as configurações para trabalhar com o banco de dados

--- src/route.js
+++ src/route.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const questionController = require('./controllers/questionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router() //para iniciar a rota dentro do node
 
 route.get('/', (req, res) => res.render('index', { page: 'enter-room' })) // é utilizado o caminho para o server entender qual tem que renderizar na tela
 route.get('/create-pass', (req, res) => res.render('index', { page: 'create-pass' }))
-route.get('/room', (req, res) => res.render('room'))
+route.get('/room/:room', (req, res) => res.render('room'))
 
 // Formato que o formulario de dentro da modal tem que passar a informação
-route.post('/room/:room/:question/:action', questionController.index)
+route.post('/question/:room/:question/:action', questionController.index)
+route.post('/create-room', RoomController.create)
 
 module.exports = route
