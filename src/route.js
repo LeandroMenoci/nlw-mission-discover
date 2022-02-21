4d1404f3a4070cafc3dfdca8d169f953fc7f2a07
LeandroMenoci <menoci.leandro@gmail.com>
2022-02-21 17:25:50 -0300

    Rota dentro do index.ejs para utilizar a mesma página

--- src/route.js
+++ src/route.js
@@ -3,9 +3,9 @@ const questionController = require('./controllers/questionController')
 
 const route = express.Router() //para iniciar a rota dentro do node
 
-route.get('/', (req, res) => res.render('index')) // é utilizado o caminho para o server entender qual tem que renderizar na tela
+route.get('/', (req, res) => res.render('index', { page: 'enter-room' })) // é utilizado o caminho para o server entender qual tem que renderizar na tela
+route.get('/create-pass', (req, res) => res.render('index', { page: 'create-pass' }))
 route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
 
 // Formato que o formulario de dentro da modal tem que passar a informação
 route.post('/room/:room/:question/:action', questionController.index)
